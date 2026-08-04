7c37eb0a8c8c7a1e40db4ebb19fa695b159ce824
Imad <imadajaj00@gmail.com>
2026-08-04 13:05:38 +0100

    add user feature

--- src/main/java/com/example/project_training/controller/ProductController.java
+++ src/main/java/com/example/project_training/controller/ProductController.java
@@ -4,4 +4,12 @@ public class ProductController {
     public String hello() {
         return "Hello";
     }
+
+    public String getUsers() {
+        return "list users";
+    }
+
+    public String deleteUser() {
+        return "delete user";
+    }
 }
