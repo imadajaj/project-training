a0e4782f60ebb9c532ee8a276124cdf89da24780
Imad <imadajaj00@gmail.com>
2026-08-04 13:06:49 +0100

    Revert "add user feature"
    This reverts commit 7c37eb0a8c8c7a1e40db4ebb19fa695b159ce824.

--- src/main/java/com/example/project_training/controller/ProductController.java
+++ src/main/java/com/example/project_training/controller/ProductController.java
@@ -4,12 +4,4 @@ public class ProductController {
     public String hello() {
         return "Hello";
     }
-
-    public String getUsers() {
-        return "list users";
-    }
-
-    public String deleteUser() {
-        return "delete user";
-    }
 }
