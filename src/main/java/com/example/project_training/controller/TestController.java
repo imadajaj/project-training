1304dd2aad60c11168d830df73d03677eed01ac6
Imad <imadajaj00@gmail.com>
2026-08-04 15:09:44 +0100

    initial test controller

---
+++ src/main/java/com/example/project_training/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.project_training.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String getTest() {
+        return "test";
+    }
+}
